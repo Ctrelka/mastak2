bf4704e2e92229ff2f848bc1bc572f2449291a06
Ctrelka <eduard.ignatyuk.1998@mail.ru>
2019-03-27 15:49:47 +0300

    update frontend

--- assets/templates/masha.js
+++ assets/templates/masha.js
@@ -6,11 +6,6 @@ import './../common/_common.scss';
 import './../mastak-lib/loader/loader';
 import './../components/header/header';
 import './../components/footer/footer';
-import './../components/programm/programm';
-import './../components/about_spiker/about_spiker';
-import '../components/block-with-label/block-with-label';
-import '../components/block-without-label/block-without-label';
-import '../components/promokod/promokod';
 
 
 
