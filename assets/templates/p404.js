8aa0aaf71a89a0e8c311a96bcfb0c4a48f36d2fb
Ctrelka <eduard.ignatyuk.1998@mail.ru>
2019-03-28 17:43:31 +0300

    create page404 template

--- assets/templates/p404.js
+++ assets/templates/p404.js
@@ -6,3 +6,4 @@ import './../common/_common.scss';
 import './../mastak-lib/loader/loader';
 import './../components/header/header';
 import './../components/footer/footer';
+import './../components/page404/page404';
