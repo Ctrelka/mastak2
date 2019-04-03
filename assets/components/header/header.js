1460aa09a8aa6e6dac90fecbc09fd10d6ee0b043
Маша Юрлова <mariya.yurlova@gmail.com>
2019-04-03 11:39:15 +0300

    Create menu

--- assets/components/header/header.js
+++ assets/components/header/header.js
@@ -1,2 +1,23 @@
 import './header.scss';
-import $ from "jquery";
+import $ from 'jquery';
+
+$(".header__burger").click(function(){
+    $(".header__burger").toggleClass("header__burger_active");
+    $(".header__menu").toggleClass("header__menu_active");
+    $(".header__menu-title").toggleClass("header__menu-title_active");
+    $(".header__menu-list-item").toggleClass("header__menu-list-item_active-item");
+    $(".header__menu-list-item").toggleClass("active");
+    $(".header__menu-list-item_active").find( "a" ).toggleClass("header__menu-list-item_active-link-load");
+});
+
+
+
+$( ".header__menu-list-item" )
+    .mouseover(function() {
+        $(".header__menu-list-item_active").find( "a" ).removeClass("header__menu-list-item_active-link");
+        $(".header__menu-list-item_active").find( "a" ).removeClass("header__menu-list-item_active-link-load");
+
+    })
+    .mouseout(function() {
+        $(".header__menu-list-item_active").find( "a" ).addClass("header__menu-list-item_active-link");
+    });
