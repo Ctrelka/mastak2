90989f51284d6eff6b0c77694ee1ce9f525d3b85
Маша Юрлова <mariya.yurlova@gmail.com>
2019-04-03 12:41:08 +0300

    Create title

---
+++ assets/components/titleLittle/titleLittle.js
@@ -0,0 +1,1 @@
+import './titleLittle.scss';
--- assets/templates/masha.js
+++ assets/templates/masha.js
@@ -7,5 +7,8 @@ import './../mastak-lib/loader/loader';
 import './../components/header/header';
 import './../components/footer/footer';
 import './../components/footer-line/footer-line';
+import './../components/titleLittle/titleLittle';
+import './../components/titleMedium/titleMedium';
+import './../components/titleBig/titleBig';
 
 
