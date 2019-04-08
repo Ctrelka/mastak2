d2f8f2131978dba71a00362987e25eabdcd64e8e
Маша Юрлова <mariya.yurlova@gmail.com>
2019-04-08 13:22:41 +0300

    Create contact

--- assets/templates/contact.js
+++ assets/templates/contact.js
@@ -9,3 +9,4 @@ import './../components/footer/footer';
 import './../components/footer-line/footer-line';
 import '../components/title/title';
 import '../components/contact/contact';
+import '../components/button/button';
