8076afafdcfdd9eea23ba8b451cfdbc7c6ebd620
Androsov <gilgust090891@gmail.com>
2019-04-12 11:56:54 +0300

    team ready

--- template-team.php
+++ template-team.php
@@ -5,14 +5,16 @@
 	get_header( "red" );
 	$text = carbon_get_post_meta(get_the_ID(), "crb_text");
 	$button_link = carbon_get_post_meta(get_the_ID(), "crb_button_link");
+	$top_img = carbon_get_post_meta(get_the_ID(), "top_img");
+	$middle_img = carbon_get_post_meta(get_the_ID(), "middle_img");
+	$button_img = carbon_get_post_meta(get_the_ID(), "button_img");
 
 	get_template_part( '/core/views/headerView' ); ?>
     <main class="main">
         <div class="team">
             <div class="team__inner">
                 <div class="titleBig">
-                    <h1 class="titleBig__text titleBig__color titleBig__color_white">team
-                    </h1>
+                    <h1 class="titleBig__text titleBig__color titleBig__color_white"><?= get_the_title(); ?></h1>
                 </div>
                 <div class="team__inner-container">
                     <div class="team__text-container">
@@ -24,15 +26,15 @@
                     <div class="team__image-container">
                         <div class="team__image-container-team-first">
                             <img class="team__image-container-team-first-image"
-                                 src="/wp-content/themes/mastak2/src/icons/team1.0071aa.png" alt="Team 1" title=""/>
+                                 src="<?= $button_img; ?>" alt="Team 1" title=""/>
                         </div>
                         <div class="team__image-container-team-second">
                             <img class="team__image-container-team-second-image"
-                                 src="/wp-content/themes/mastak2/src/icons/team2.d17e72.png" alt="Team 2" title=""/>
+                                 src="<?= $middle_img; ?>" alt="Team 2" title=""/>
                         </div>
                         <div class="team__image-container-team-third">
                             <img class="team__image-container-team-third-image"
-                                 src="/wp-content/themes/mastak2/src/icons/team3.ae9b6f.png" alt="Team 3" title=""/>
+                                 src="<?= $top_img; ?>" alt="Team 3" title=""/>
                         </div>
                     </div>
                 </div>
