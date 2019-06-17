bed8f962135fb6ad2b7a76678192550eef1c2c6a
zankoav <zankoav@mail.ru>
2019-06-17 12:53:36 +0300

    load_big_img

--- functions.php
+++ functions.php
@@ -36,3 +36,12 @@
 	require_once __DIR__ . '/core/customTypes/portfolioTax.php';
 	require_once __DIR__ . '/core/carbon/portfolioTermMeta.php';
 	require_once __DIR__ . '/core/carbon/portfolioSettings.php';
+
+
+ function wpb_image_editor_default_to_gd( $editors ) {
+     $gd_editor = 'WP_Image_Editor_GD';
+     $editors = array_diff( $editors, array( $gd_editor ) );
+     array_unshift( $editors, $gd_editor );
+     return $editors;
+ }
+ add_filter( 'wp_image_editors', 'wpb_image_editor_default_to_gd' );
